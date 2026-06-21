c50a792010905ca04635fc4007149203fc20bf37
minimaple <minimapletinytools@gmail.com>
2026-06-21 14:59:01 -0700

    fix issues

--- patterns/patternbook_patterns.py
+++ patterns/patternbook_patterns.py
@@ -294,22 +294,22 @@ def create_patternbook_example_patternbook() -> PatternBook:
 
 
 def _make_short_post(center):
-    return Frame(cut_timbers=[CutTimber(timber_from_directions(length=feet(4), size=create_v2(inches(4), inches(4)), bottom_position=center, length_direction=create_v3(Integer(0), Integer(0), Integer(1)), width_direction=create_v3(Integer(1), Integer(0), Integer(0)), ticket="short_post"), cuts=[])], ticket="short_post_frame")
+    return Frame(cut_timbers=[CutTimber(timber_from_directions(length=feet(4), size=create_v2(inches(4), inches(4)), bottom_position=center, length_direction=create_v3(Integer(0), Integer(0), Integer(1)), width_direction=create_v3(Integer(1), Integer(0), Integer(0)), ticket="short_post"), cuts=[])], name="Short Post")
 
 def _make_tall_post(center):
-    return Frame(cut_timbers=[CutTimber(timber_from_directions(length=feet(8), size=create_v2(inches(6), inches(6)), bottom_position=center, length_direction=create_v3(Integer(0), Integer(0), Integer(1)), width_direction=create_v3(Integer(1), Integer(0), Integer(0)), ticket="tall_post"), cuts=[])], ticket="tall_post_frame")
+    return Frame(cut_timbers=[CutTimber(timber_from_directions(length=feet(8), size=create_v2(inches(6), inches(6)), bottom_position=center, length_direction=create_v3(Integer(0), Integer(0), Integer(1)), width_direction=create_v3(Integer(1), Integer(0), Integer(0)), ticket="tall_post"), cuts=[])], name="Tall Post")
 
 def _make_wide_post(center):
-    return Frame(cut_timbers=[CutTimber(timber_from_directions(length=feet(6), size=create_v2(inches(8), inches(8)), bottom_position=center, length_direction=create_v3(Integer(0), Integer(0), Integer(1)), width_direction=create_v3(Integer(1), Integer(0), Integer(0)), ticket="wide_post"), cuts=[])], ticket="wide_post_frame")
+    return Frame(cut_timbers=[CutTimber(timber_from_directions(length=feet(6), size=create_v2(inches(8), inches(8)), bottom_position=center, length_direction=create_v3(Integer(0), Integer(0), Integer(1)), width_direction=create_v3(Integer(1), Integer(0), Integer(0)), ticket="wide_post"), cuts=[])], name="Wide Post")
 
 def _make_small_beam(center):
-    return Frame(cut_timbers=[CutTimber(timber_from_directions(length=feet(8), size=create_v2(inches(2), inches(4)), bottom_position=center, length_direction=create_v3(Integer(1), Integer(0), Integer(0)), width_direction=create_v3(Integer(0), Integer(1), Integer(0)), ticket="small_beam"), cuts=[])], ticket="small_beam_frame")
+    return Frame(cut_timbers=[CutTimber(timber_from_directions(length=feet(8), size=create_v2(inches(2), inches(4)), bottom_position=center, length_direction=create_v3(Integer(1), Integer(0), Integer(0)), width_direction=create_v3(Integer(0), Integer(1), Integer(0)), ticket="small_beam"), cuts=[])], name="Small Beam")
 
 def _make_medium_beam(center):
-    return Frame(cut_timbers=[CutTimber(timber_from_directions(length=feet(10), size=create_v2(inches(4), inches(6)), bottom_position=center, length_direction=create_v3(Integer(1), Integer(0), Integer(0)), width_direction=create_v3(Integer(0), Integer(1), Integer(0)), ticket="medium_beam"), cuts=[])], ticket="medium_beam_frame")
+    return Frame(cut_timbers=[CutTimber(timber_from_directions(length=feet(10), size=create_v2(inches(4), inches(6)), bottom_position=center, length_direction=create_v3(Integer(1), Integer(0), Integer(0)), width_direction=create_v3(Integer(0), Integer(1), Integer(0)), ticket="medium_beam"), cuts=[])], name="Medium Beam")
 
 def _make_large_beam(center):
-    return Frame(cut_timbers=[CutTimber(timber_from_directions(length=feet(12), size=create_v2(inches(6), inches(8)), bottom_position=center, length_direction=create_v3(Integer(1), Integer(0), Integer(0)), width_direction=create_v3(Integer(0), Integer(1), Integer(0)), ticket="large_beam"), cuts=[])], ticket="large_beam_frame")
+    return Frame(cut_timbers=[CutTimber(timber_from_directions(length=feet(12), size=create_v2(inches(6), inches(8)), bottom_position=center, length_direction=create_v3(Integer(1), Integer(0), Integer(0)), width_direction=create_v3(Integer(0), Integer(1), Integer(0)), ticket="large_beam"), cuts=[])], name="Large Beam")
 
 def _make_small_box(center):
     size = inches(2)
