162b5b1e6af442f6fad1fa5ddc60baabf1fa8123
minimaple <minimapletinytools@gmail.com>
2026-06-21 19:44:03 -0700

    Release kumiki v0.3.3 and kigumi v0.3.3

--- kumiki/__init__.py
+++ kumiki/__init__.py
@@ -5,7 +5,7 @@ Based on the API specification in morenotes.md
 This is the main entry point that imports and re-exports all kumiki functionality.
 """
 
-__version__ = "0.3.2"
+__version__ = "0.3.3"
 
 # Import everything from the organized modules
 from .rule import *
